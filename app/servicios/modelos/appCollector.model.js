38c5622e14181e5c14a199399d8bda9231fcd6be
d.blanco <d.blanco@scc.uned.es>
2025-01-08 12:12:32 +0100

    Añadir esquemas mongoose

--- app/servicios/modelos/appCollector.model.js
+++ app/servicios/modelos/appCollector.model.js
@@ -3,31 +3,73 @@ const Schema = mongoose.Schema;
 
 const apkSchema  = new Schema(
 	{
-		package: String,
-	  }, { collection: 'apks' }
+		_id: { $oid: String, required: true, unique: true },
+		name: { type: String, required: true, unique: true },
+		package: { type: String, required: true },
+		category: String,
+		adSupported: Boolean,
+		containsAds: Boolean,
+		contentRating: String,
+		contentRatingDescription: String,
+		currency: String,
+		description: String,
+		developer: String,
+		developerEmail: String,
+		developerWebsiteUrl: String,
+		free: Boolean,
+		genre: String,
+		genreId: String,
+		googlePlayUrl: { type: String, unique: true },
+		headerImageUrl: String,
+		histogram: [Number],
+		iconUrl: String,
+		installs: String,
+		price: Number,
+		privacyPolicyUrl: String,
+		ratings: Number,
+		realInstalls: Number,
+		released: String,
+		reviews: Number,
+		score: Number,
+		screenshotsUrls: [String],
+		title: String,
+		videoImageUrl: String,
+		videoUrl: String,
+	}, { collection: 'apks' }
 );
 
 const tplSchema  = new Schema(
 	{
-		package: String,
-	  }, { collection: 'tpls' }
+		_id: { $oid: String, required: true, unique: true },
+		name: { type: String, required: true},
+		package: { type: String, required: true, unique: true },
+		continuousIntegrationUrl: String,
+		descripcion: String,
+		issueTrackerUrl: String,
+		lastTimeChecked: String,
+		licenses: [String],
+		ossindexVulnerabilities: String,
+		pomFile: String,
+		projectUrl: String,
+		published: String,
+		size: String,
+		sourceControlUrl: String,
+	}, { collection: 'tpls' }
 );
-
 
 const versionSchema  = new Schema(
 	{
-		package: String,
-	  }, { collection: 'versions' }
+		_id: { $oid: String, required: true, unique: true },
+		parentId: { $oid: String, required: true },
+		type: { type: String, required: true },
+		versionCode: { type: String, required: true },
+		downloadUrl: { type: String, required: true },
+	}, { collection: 'versions' }
 );
-
-  
-
-// pruebaSchema.index({codigo: 1}, {unique: true});
 
 const apks = mongoose.model('apks', apkSchema)
 const tpls = mongoose.model('tpls', tplSchema)
 const versions = mongoose.model('versions', versionSchema); 
-
 
 const tablas = {
 	apks: {
