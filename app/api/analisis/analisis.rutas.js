1279e39692dd4a2b055290fc389437e5e248ed6a
d.blanco <d.blanco@scc.uned.es>
2025-02-24 11:09:38 +0100

    Añadir CRUD análisis estático

--- app/api/analisis/analisis.rutas.js
+++ app/api/analisis/analisis.rutas.js
@@ -1,8 +1,10 @@
 'use strict'
 const mobsf_api = require('./mobsf.api.js')
 const privado_api = require('./privado.api.js')
+const estatico_api = require('./estatico.api.js')
 
 module.exports = (app) => {    
     mobsf_api(app, '/api/analisis/mobsf')
     privado_api(app, '/api/analisis/privado')
+    estatico_api(app, '/api/analisis/estatico')
 }
---
+++ app/api/analisis/estatico.api.js
@@ -0,0 +1,42 @@
+'use strict';
+const ESTATICO = require('../../modAnalisis/estatico.js');
+
+module.exports = (app, ruta) => {
+
+    /**
+     * @swagger
+     * tags:
+     *   name: Estático
+     *   description: Rutas para gestionar el análisis estático de las APKs.
+     *
+     * /api/estatico/search:
+     *   post:
+     *     summary: Define la ruta para buscar una APK con opciones de búsqueda detalladas.
+     *     tags: [Estático]
+     *     responses:
+     *       200:
+     *         description: Los detalles del análisis estático de la/s APK/s buscadas.
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 $ref: '#/components/schemas/Estatico'
+     */
+    app.route(`${ruta}/search`)
+    .post((req, res, next) => {
+        const opciones = req.body;
+
+        if (typeof opciones !== 'object') {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Las opciones deben ser un objeto válido.',
+                datos: []
+            });
+        }
+
+        ESTATICO.leerCampo(opciones)
+            .then(result => res.status(200).json(result))
+            .catch(err => next(err));
+    });
+};
